80ac73d4d29b7890bb36d8edb1a64d9bcd6d84c7
Sarah Good <sarah.elizabeth.good@gmail.com>
2025-02-01 12:28:08 -0800

    Function to let computer choose square

--- scripts.js
+++ scripts.js
@@ -4,7 +4,25 @@ boxes.forEach(box => {
     box.addEventListener('click', function handleClick(event) {
         console.log('clicky', event);
         event.currentTarget.innerHTML = 'X';
+        event.currentTarget.classList.add('player');
+
+        computerTurn();
     });
 });
 
-console.log(boxes.length);
+function computerTurn() {
+    
+    computerTarget = Math.floor(Math.random() * 8);
+    computerChoice = boxes[computerTarget];
+
+    while (computerChoice.classList.contains('player') || computerChoice.classList.contains('computer')) {
+        computerTarget = Math.floor(Math.random() * 8);
+        computerChoice = boxes[computerTarget];
+    }
+
+    computerChoice.innerHTML = 'O';
+    computerChoice.classList.add('computer');
+
+
+}
+
